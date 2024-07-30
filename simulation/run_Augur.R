825acd39b6788b7e3b50ee9cdb04e784877cf705
Yingxin Lin <yingxinlindsay@gmail.com>
2024-07-29 23:06:00 -0400

    delete old files

--- simulation/run_Augur.R
+++
@@ -1,6 +0,0 @@
-library(Augur)
-library(SingleCellExperiment)
-sce_sim <- readRDS("data/sce_sim_test.rds")
-sce_sim$cell_type <- sce_sim$scClassify_tumour_prediction_coarse
-augur <- calculate_auc(sce_sim, label_col = "Condition", cell_type_col = "cell_type", n_threads = 1)
-augur$AUC
